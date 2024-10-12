09a1e96bdec19377f155752dbe2a9c94ef8b336f
Harshit Jain <harshitostwal1234@gmail.com>
2024-10-13 02:47:52 +0530

    Javascript Course Leson - 1

---
+++ 01/Readme.txt
@@ -0,0 +1,3 @@
+In JS There Are Two Types Of Script
+    - Interal JS (HTML)
+    - External JS (.js Files)
---
+++ 01/script.js
@@ -0,0 +1,3 @@
+console.log("Welcome To External Js Course");
+console.log("Harshit Jain");
+console.log(1+1);
